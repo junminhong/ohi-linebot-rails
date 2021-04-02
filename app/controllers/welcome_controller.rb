f5966610010bcc960b86f63e995ba6228a713ad8
junminhong <junminhong1110@gmail.com>
2021-04-02 20:30:10 +0800

    測試鸚鵡說話

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,21 @@
 class WelcomeController < ApplicationController
-    def webhook
-        render nothing: true, status: :ok
-    end
+	def webhook
+		reply_token = params['events'][0]['replyToken']
+
+    response_message = {
+      type: "text",
+      text: params["events"].first["message"]["text"]
+    }
+
+    client.reply_message(reply_token, response_message)
+		# render nothing: true, status: :ok
+	end
+	
+	def client
+		@client ||= Line::Bot::Client.new { |config|
+			config.channel_id = ENV["LINE_CHANNEL_ID"]
+			config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+			config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+		}
+	end
 end
